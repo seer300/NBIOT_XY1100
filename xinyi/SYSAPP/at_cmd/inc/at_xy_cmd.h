2db2ea5b4f1d8868437430606c6c5695ddfd63d9
MGlinGF <lingengfeng@meigsmart.com>
2023-10-30 10:17:58 +0800

    发布版本SLM130X.0.2.3.C21T18S1030_M003_XY1100、SLM160X.0.2.3.C21T18S1030_M003_XY1100、SLM130G.0.2.3.C21T18S1030_M003_XY1100

--- xinyi/SYSAPP/at_cmd/inc/at_xy_cmd.h
+++ xinyi/SYSAPP/at_cmd/inc/at_xy_cmd.h
@@ -29,8 +29,8 @@
 #if  MODULE_130
 
 #define MODULE 		"SLM130X"
-#define TVERSION 	"T17"
-#define SDATE		"S1017"
+#define TVERSION 	"T18"
+#define SDATE		"S1030"
 #define SDKVERSION	"0.2.3.C21"  /*20230224 patch03*/
 #define MVERSION	"M003"
 #define SI_NUM		HAL_GPIO_PIN_NUM_13
@@ -38,8 +38,8 @@
 #else
 
 #define MODULE 		"SLM160X"
-#define TVERSION 	"T17"
-#define SDATE		"S1017"
+#define TVERSION 	"T18"
+#define SDATE		"S1030"
 #define SDKVERSION	"0.2.3.C21"
 #define MVERSION	"M003"
 #define SI_NUM		HAL_GPIO_PIN_NUM_11
